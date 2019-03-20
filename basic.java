518be94bca9a5bd681d9b3022266f5c7056eadce
nikhilfurious <aashishchandak1999@gmail.com>
2019-03-20 22:17:13 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.Scanner;
+
+ class checker {
+void check(int n){
+if(n>0)
+{System.out.println("POSITIVE");}
+else
+{System.out.println("NEGATIVE");}
+}
+}
+
+public class basic{
+public static void main(String args[])
+{
+Scanner scan = new Scanner(System.in);
+System.out.println("Enter the number to be checked:");
+int n = scan.nextInt();
+checker o = new checker();
+o.check(n);
+} 
+}
