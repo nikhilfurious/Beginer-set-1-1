caedd4f4750991ef485768a46825d6f735722559
nikhilfurious <aashishchandak1999@gmail.com>
2019-03-21 13:48:09 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 
  class checker {
 void fun(int n){
-if(n%2==0)
+if((n%2==0)&&(n>0))
 {System.out.println("EVEN");}
-else 
-{
-System.out.println("INVALID");
-}
-}
+else if((n%2!=0)&&(n>0))
+{System.out.println("ODD");}
+else
+{System.out.println("INVALID");}
 }
 
 public class oddeven{
