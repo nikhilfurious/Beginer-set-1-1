146f6fce19a998282c4196a694c623bf78a402c0
nikhilfurious <aashishchandak1999@gmail.com>
2019-04-09 10:30:22 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -1,23 +1,28 @@
 import java.lang.*;
 import java.util.Scanner;
 
- class checker {
-void fun(int n){
-if((n%2==0)&&(n>0))
-{System.out.println("EVEN");}
-else if((n%2!=0)&&(n>0))
-{System.out.println("ODD");}
-else
-{System.out.println("INVALID");}
+class checker {
+void check(int n)
+{
+    if(n>0)
+    {
+        if(n%2==0)
+        {System.out.println("Even");}
+        else
+        {System.out.println("Odd");}
+        
+    }
+    else
+    {System.out.println("invalid");}
+}
 }
 
-public class oddeven{
+class Main{
 public static void main(String args[])
 {
 Scanner scan = new Scanner(System.in);
-System.out.println("INPUT:");
 int n = scan.nextInt();
 checker o = new checker();
-o.fun(n);
+o.check(n);
 } 
 }
